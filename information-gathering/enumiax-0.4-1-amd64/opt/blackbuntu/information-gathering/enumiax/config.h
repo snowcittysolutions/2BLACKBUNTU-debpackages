22aafbf835f5d7a65213e52035e5b22ee549710b
neoslab <github@neoslab.com>
2022-01-19 15:36:26 -0500

    Update project repository

--- information-gathering/enumiax-0.4-1-amd64/opt/blackbuntu/information-gathering/enumiax/config.h
+++
@@ -1,9 +0,0 @@
-
-/* Auto-Generated Passphrase Options */
-
-/* Minimum & maximum default passphrase length in characters */
-#define MIN_USER_LEN 1
-#define MAX_USER_LEN 8
-#define MIN_PASS_LEN 1
-#define MAX_PASS_LEN 6
-
--- information-gathering/toutatis-1.0.1-amd64/opt/blackbuntu/information-gathering/toutatis/toutatis/__init__.py
+++
@@ -1,1 +0,0 @@
-from toutatis.core import *
